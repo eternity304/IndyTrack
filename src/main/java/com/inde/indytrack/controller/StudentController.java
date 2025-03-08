87950e5b09978c6e2919f3c88d5dc40c23826347
Hayden Choi <hyj.choi@mail.utoronto.ca>
2025-03-08 17:23:17 -0500

    Add POST functionality in StudentController

--- src/main/java/com/inde/indytrack/controller/StudentController.java
+++ src/main/java/com/inde/indytrack/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.inde.indytrack.controller;
 
+import com.inde.indytrack.dto.RegisterDTO;
 import com.inde.indytrack.entity.Student;
 import com.inde.indytrack.exception.StudentNotFoundException;
 import com.inde.indytrack.repository.StudentRepository;
@@ -30,6 +31,16 @@ public class StudentController {
     Student retrieveStudent(@PathVariable("id") Long studentId) {
         return repository.findById(studentId)
                 .orElseThrow(() -> new StudentNotFoundException(studentId));
+    }
+
+    @PostMapping
+    Student createStudent(@RequestBody RegisterDTO newStudent) {
+        Student student = new Student();
+        student.setFirstName(newStudent.getFirstName());
+        student.setLastName(newStudent.getLastName());
+        student.setEmail(newStudent.getEmail());
+        student.setPassword(newStudent.getPassword());
+        return repository.save(student);
     }
 
     @PutMapping("/{id}")
